620b69c54ef54eda7b33150e157e2e779abff1e0
Adam Smith <>
2024-01-31 12:34:47 +0000

    feat: add page and navbar components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,81 +1,13 @@
-import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  signal,
-} from '@angular/core';
-import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
-import {
-  FormBuilder,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { Store } from '@ngrx/store';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
-import { Character } from './models/rick-morty.models';
-import { getCharacters } from './store/actions/rick-morty.actions';
-import {
-  selectCharactersData,
-  selectCharactersError,
-  selectCharactersLoading,
-} from './store/selectors/rick-morty.selectors';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
-  protected characters = toSignal(this.store.select(selectCharactersData));
-  protected isLoading = toSignal(this.store.select(selectCharactersLoading));
-  protected error = toSignal(this.store.select(selectCharactersError));
-  protected favouriteCharacter = signal<Character | null>(null);
-
-  protected form: FormGroup = this.formBuilder.group({
-    character: [],
-  });
-
-  constructor(private store: Store, private formBuilder: FormBuilder) {
-    this.watchCharacterInput();
-  }
-
-  public ngOnInit(): void {
-    this.store.dispatch(getCharacters({}));
-  }
-
-  private watchCharacterInput(): void {
-    this.form
-      .get('character')
-      ?.valueChanges.pipe(
-        takeUntilDestroyed(),
-        distinctUntilChanged(),
-        debounceTime(500)
-      )
-      .subscribe((value: Character | string) => {
-        if (typeof value === 'string') {
-          this.store.dispatch(getCharacters({ searchTerm: value }));
-        } else {
-          this.favouriteCharacter.set(value);
-        }
-      });
-  }
-
-  protected displayFn(character: Character): string {
-    return character?.name || '';
-  }
-}
+export class AppComponent {}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {}
