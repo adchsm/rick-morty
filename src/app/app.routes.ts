414534d61e5e293b3507cb3f8d9de239b62e200f
Adam Smith <>
2024-01-31 17:26:45 +0000

    chore: add title and style fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { CharacterPageComponent } from './pages/character-page/character-page.component';
 
-export const routes: Routes = [{ path: '', component: CharacterPageComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CharacterPageComponent,
+    title: 'The Rick & Morty Character Database',
+  },
+];
